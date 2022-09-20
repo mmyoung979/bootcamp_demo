5ac8f768c5e39ba7094b382b772718bd49f43216
Matt Young <mmyoung979@gmail.com>
2022-09-19 22:36:03 -0500

    Add comments

--- app.py
+++ app.py
@@ -1,15 +1,28 @@
+# Python imports
 import os
+
+# Third party imports
 from flask import Flask, request
 import requests
 
+# Global variables
 app = Flask(__name__)
 AUTH = f"api_key={os.environ.get('api_key')}"
 
 @app.route("/", methods=["GET", "POST", "PATCH", "DELETE"])
 def cats():
     if request.method == "GET":
+        # How many links to retrieve
         limit = request.args.get("limit", 1)
+
+        # API request URL
         url = f"https://api.thecatapi.com/v1/images/search?limit={limit}&{AUTH}"
+
+        # API results
         response = requests.get(url).json()
+
+        # Parse and return API results
         return [cat["url"] for cat in response]
+
+    # Return the type of request if not GET request
     return {"message": f"{request.method} request"}
